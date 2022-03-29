a3567c307913779673a63c38c810a485ab5945e0
chlbri <bri_lvi@icloud.com>
2022-03-29 18:54:01 +0000

    style: beauty

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,34 @@ import { FC } from 'react';
 
 type CardProps = {
   children?: number;
+  color?: string;
+  backgroundColor?: string;
+  opacity?: number;
 };
 
-const Card: FC<CardProps> = ({ children }) => {
-  const backgroundColor = children ? 'maroon' : 'black';
-  const opacity = children ? 0.8 : 0.35;
+const Card: FC<CardProps> = ({
+  children,
+  color = 'white',
+  opacity = 0.35,
+  backgroundColor = 'black',
+}) => {
   return (
     <div
-      className="rounded-2xl shadow-2xl text-5xl flex text-gray-50 items-center justify-center font-bold"
-      style={{ backgroundColor, opacity }}
+      className="rounded sm:rounded-md lg:rounded-xl xl:rounded-2xl shadow-md md:shadow-2xl text-5xl flex items-center justify-center font-bold"
+      style={{ color, opacity, backgroundColor }}
     >
       {children}
     </div>
   );
 };
 
+const GRID_SIZE = 4;
+
 const Index: FC = () => {
   return (
     <div className="h-screen pt-5 flex flex-col items-center text-7xl text-yellow-900 font-semibold bg-yellow-50">
       <span>2048 !</span>
-      <div className="relative w-5/12 m-auto aspect-square bg-yellow-900/20 rounded-xl overflow-hidden">
+      <div className="relative w-4/5 sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-5/12 m-auto aspect-square rounded-xl overflow-hidden">
         <div className="absolute inset-0 ">
           <Image
             layout="fill"
@@ -32,9 +40,9 @@ const Index: FC = () => {
             src="/images/wood-small.jpg"
           />
         </div>
-        <div className="w-full h-full grid grid-cols-4 gap-4 p-4 z-50">
-          {Array.from({ length: 16 }).map(() => (
-            <Card key={nanoid()}>{undefined}</Card>
+        <div className="w-full h-full grid grid-cols-4 gap-2 p-2 sm:gap-3 sm:p-3 lg:gap-4 lg:p-4 z-50 relative">
+          {Array.from({ length: Math.pow(GRID_SIZE, 2) }).map(() => (
+            <Card key={nanoid()} />
           ))}
         </div>
       </div>
